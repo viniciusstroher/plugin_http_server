02d5e8c94d7db3ba2cb4256bcf1b602ccdf3e09f
vinicius~~ <viniciusferreirwk@gmail.com>
2017-08-12 17:57:24 -0300

    fix

--- src/android/App.java
+++ src/android/App.java
@@ -18,7 +18,12 @@ public class App extends NanoHTTPD {
     @Override
     public Response serve(IHTTPSession session) {
         session.set
-        String msg = "<html><body><h1>Hello server</h1>\n";
+
+        if (!session.getUri().equalsIgnoreCase("/")){
+            return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/json", "{api:true}");
+        }
+
+        /*String msg = "<html><body><h1>Hello server</h1>\n";
         Map<String, String> parms = session.getParms();
         if (parms.get("username") == null) {
             msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
@@ -26,5 +31,15 @@ public class App extends NanoHTTPD {
             msg += "<p>Hello, " + parms.get("username") + "!</p>";
         }
         return newFixedLengthResponse(msg + "</body></html>\n");
+
+        if (!session.getUri().equalsIgnoreCase("/info"))
+            return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "Resource not found.");
+
+        if (!session.getMethod().equals(Method.GET))
+            return newFixedLengthResponse(Response.Status.METHOD_NOT_ALLOWED, "text/plain", "Method not allowed.");
+
+        String response = "{\"working\":{}}";
+        return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "application/json", response);
+        */
     }
 }
