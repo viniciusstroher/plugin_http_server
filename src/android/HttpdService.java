e025da0575851f5047eacb9f31d842004ecb772b
vinicius~~ <viniciusferreirwk@gmail.com>
2017-08-12 15:04:28 -0300

    fix

--- src/android/HttpdService.java
+++ src/android/HttpdService.java
@@ -15,12 +15,17 @@ public class HttpdService extends Service {
         super.onCreate();
     }
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) throws IOException{
-        //DataSync d = new DataSync(HttpdService.this);
-        //d.syncPendingNotes();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try{
+            super.onStartCommand(intent, flags, startId);
+            //DataSync d = new DataSync(HttpdService.this);
+            //d.syncPendingNotes();
 
-        server = new App();
-        return START_NOT_STICKY;
+            server = new App();
+            return START_NOT_STICKY;
+        }catch(Exception e){
+            Log.i(HttpdService.LOG_TAG,"Error onStartCommand: "+e.getMessage());
+        }
     }
     
     @Override
