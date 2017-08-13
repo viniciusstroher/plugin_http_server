b5acfd652c0e08413ae0496bafd1c4a83f2b3446
vinicius~~ <viniciusferreirwk@gmail.com>
2017-08-12 22:29:22 -0300

    fix

--- src/android/App.java
+++ src/android/App.java
@@ -19,22 +19,22 @@ public class App extends NanoHTTPD {
     public Response serve(IHTTPSession session) {
         //session.set
         Method method = session.getMethod();
-        String hookReturn = "{api:'no hooks'}";
+        String hookReturn = "{\"api\":\"no hooks\"}";
         Map<String, String> headers = session.getHeaders();
         
         // se nao for post, n aceita o request
         if (!Method.POST.equals(method)) {
-            hookReturn="{api:'use post method'}";
+            hookReturn="{\"api\":\"use post method\"}";
             return newFixedLengthResponse(Response.Status.OK, "text/json", hookReturn);
         }
 
         if(headers.get("Api-Key") == null){
-            hookReturn="{api:'Api-Key not exists in header'}";
+            hookReturn="{\"api\":\"Api-Key not exists in header\"}";
             return newFixedLengthResponse(Response.Status.OK, "text/json", hookReturn);
         }
 
         if(!headers.get("Api-Key").equals(senha)){
-            hookReturn="{api:'Api-Key wrong '}";
+            hookReturn="{\"api\":\"Api-Key wrong\"}";
             return newFixedLengthResponse(Response.Status.OK, "text/json", hookReturn);
         }
 
@@ -42,7 +42,7 @@ public class App extends NanoHTTPD {
         Httpd.pluginWebView.loadUrl("javascript:!Array.isArray(window.httpd.requests[\""+session.getUri()+"\"]) ? window.httpd.requests[\""+session.getUri()+"\"] = [] : null ;");                    
         Httpd.pluginWebView.loadUrl("javascript:window.httpd.requests[\""+session.getUri()+"\"].push({retorno:1}) ;");                    
           
-        hookReturn = "{api:'ok'}";
+        hookReturn = "{\"api\":\"ok\"}";
         return newFixedLengthResponse(Response.Status.OK, "text/json", hookReturn);
         
     }
