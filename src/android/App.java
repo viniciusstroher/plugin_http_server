66223757e613b3379849b64ef891808dc2aae088
vinicius~~ <viniciusferreirwk@gmail.com>
2017-08-22 21:28:41 -0300

    fix

--- src/android/App.java
+++ src/android/App.java
@@ -125,7 +125,7 @@ public class App extends NanoHTTPD {
             // Vibrate for 500 milliseconds
             v.vibrate(500);
             
-            Intent i = new Intent("android.intent.action.MAIN");
+            Intent i = new Intent(Intent.ACTION_MAIN);
 
             NotificationCompat.Builder b = new NotificationCompat.Builder(Httpd.pluginContext);
             PendingIntent contentIntent = PendingIntent.getActivity(Httpd.pluginContext, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
